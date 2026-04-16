88adb5972769951fea13c482751cbf8f9a93ed18
foxnett77 <foxnett@gmail.com>
2026-04-16 17:44:18 +0200

    Add files via upload

---
+++ next.config.mjs
@@ -0,0 +1,19 @@
+import withPWAInit from "@ducanh2912/next-pwa";
+
+const withPWA = withPWAInit({
+  dest: "public",
+  disable: process.env.NODE_ENV === "development",
+});
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  }
+};
+
+export default withPWA(nextConfig);
